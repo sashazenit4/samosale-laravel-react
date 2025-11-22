72bd13953dea21406e0ff8e3b3da330d1a628ab4
Aleksandr.KHolin <Aleksandr.KHolin@norbit.ru>
2025-11-22 16:36:54 +0300

    feat(payments): add handles for getting payments by telegram id

--- routes/payments.php
+++ routes/payments.php
@@ -5,4 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'api'])->group(function () {
     Route::apiResource('payments', PaymentController::class);
     Route::get('payments/stats', [PaymentController::class, 'stats']);
+    Route::get('payments/telegram/{telegramId}', [PaymentController::class, 'byTelegramId']);
+    Route::get('payments/telegram/{telegramId}/stats', [PaymentController::class, 'statsByTelegramId']);
+    Route::get('payments/telegram/{telegramId}/{paymentId}', [PaymentController::class, 'paymentByTelegramId']);
 });
