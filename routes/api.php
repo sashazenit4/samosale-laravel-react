c659d41f176d6e7fa45ef9d19b75d4e315368a63
Aleksandr.KHolin <Aleksandr.KHolin@norbit.ru>
2025-11-23 15:41:34 +0300

    feat(loyalty-program): remove legacy loyalty program realization

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ Route::post('/login', [ApiAuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [ApiAuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->get('/user', [ApiAuthController::class, 'user']);
 
-require __DIR__.'/loyalty.php';
 require __DIR__.'/clients.php';
 require __DIR__.'/tariffs.php';
 require __DIR__.'/bikes.php';
