4ef7ae918cb93aaf472ae3ceced6c22a4643d1ea
Aleksandr.KHolin <Aleksandr.KHolin@norbit.ru>
2025-11-25 01:05:39 +0300

    feat(transactions): schedule tasks for transactions pay status check

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Проверка статусов транзакций каждые 5 минут
+Schedule::command('transactions:check-multiple-statuses')
+    ->everyFiveMinutes()
+    ->withoutOverlapping()
+    ->appendOutputTo(storage_path('logs/transaction-statuses.log'));
+
+// Проверка просроченных транзакций каждую минуту
+Schedule::command('transactions:check-expired')
+    ->everyMinute()
+    ->withoutOverlapping();
